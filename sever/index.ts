e40a44f6e1aeb69a032630f8d177d466c2cce3a5
DATT-2003 <dattien52003@gmail.com>
2024-11-23 00:21:16 +0700

    commit

--- sever/index.ts
+++ sever/index.ts
@@ -1,5 +1,31 @@
-import moduleName from 'express'
+import { Router } from 'express';
+import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import userRouter from './src/routers/user'
 dotenv.config();
 
-console.log('hello');
+
+const PORT = process.env.PORT || 3001;
+const dbURL = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.nx824.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const app = express();
+
+app.use('/auth', userRouter);
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dbURL)
+        console.log(`Connect to DB successfully!!!`)
+    } catch (error) {
+        console.log(`Can't connect to DB${error}`)
+    }
+};
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is starting at http://localhost:${PORT}`);
+        });
+    }).catch((error) => {
+        console.log(error)
+    })
+
